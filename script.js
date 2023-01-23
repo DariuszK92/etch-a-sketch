dfb188a3e6cf594c4040b9a405e8c0d0bbe47264
DariuszK92 <60391491+DariuszK92@users.noreply.github.com>
2023-01-23 23:32:59 +0100

    Add functions: grid lines

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+
+
 const paraSlider = document.getElementById("slider-value");
 const slider = document.getElementById("pixels");
+
 
 //Function that modifies paragraph value showing the number of pixels
 const showSliderValue = () => {
     paraSlider.textContent= slider.value + "x" + slider.value; 
 }
 slider.addEventListener('mousemove', showSliderValue);
+
+
+
+
 
 //Get slider value to calculate child divs width and height
 
@@ -20,22 +27,55 @@ let squareSliderValue =() =>{
 }
 slider.addEventListener('mousemove', squareSliderValue);
 
+//Creation of gridlines onload
+function initialDivs(){
+    for(let i=0; i<2500;i++){
+        const div = document.createElement("div");
+        container.appendChild(div);  
+        div.style.setProperty('width',`calc(100%/50)`);
+        div.className = "children";
+        }      
+}
 
-//Create childs for a div
-const createDivs = () => {
+window.addEventListener("load", initialDivs);
+
+//Create childs for a div by moving the slider
 const container = document.getElementById("sketch");
+const createDivs = () => {
         while(container.firstChild){
             container.removeChild(container.lastChild)
         }
        for(let i=0; i<squareSliderValue;i++){
-        const div = document.createElement("div");
-        container.appendChild(div);  
-        div.style.setProperty('width',`calc(100%/${currentSliderValue})`); 
-        }
-
+        const childDiv = document.createElement("div");
+        container.appendChild(childDiv);  
+        childDiv.style.setProperty('width',`calc(100%/${currentSliderValue})`);
+        childDiv.className = "children";
+       
+        }      
 }
+
 slider.addEventListener('mousemove', createDivs);
 
+//Removing borders from child elements
 
-const divsDrawing = document.getElementById("createdivs");
-divsDrawing.addEventListener('onclick', createDivs);
+
+function addGrid(){
+    const boxes = document.getElementsByClassName('children');
+    for (const children of boxes) {
+    children.classList.remove('noborder');
+    children.classList.add('border');
+  }
+}
+
+
+function removeGrid(){
+    const boxes = document.getElementsByClassName('children');
+    for (const children of boxes) {
+    children.classList.remove('border');
+    children.classList.add('noborder');
+  }
+}
+//Clear all
+
+//Coloring the divs
+
